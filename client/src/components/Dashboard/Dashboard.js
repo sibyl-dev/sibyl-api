1e8b0475e84a42c45c0389766c9b9371ac30c030
sergiu-ojoc <sergiu.ojoc@bytex.ro>
2020-06-30 16:53:56 +0300

    Feature importance component

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -6,6 +6,8 @@ import './Dashboard.scss';
 import Sandbox from '../Sandbox/Sandbox';
 import NotFound from '../common/NotFound';
 import Model from '../Model/Model';
+import FeatureImportance from '../Model/FeatureImportance';
+import GlobalFeatures from '../Model/GlobalFeatures';
 
 const Dashboard = () => {
   return (
@@ -14,6 +16,8 @@ const Dashboard = () => {
         <Route path="/details" component={Details} />
         <Route path="/sandbox" exact component={Sandbox} />
         <Route path="/model" component={Model} />
+        <Route path="/global-feature-importance" component={FeatureImportance} />
+        <Route path="/feature-distribution" component={GlobalFeatures} />
         <Route path="/" exact component={Score} />
         <Route path="*" component={NotFound} />
       </Switch>
