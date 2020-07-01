02a7d07507ef05be8f65159086c449fc14452406
dyuliu <ustdongyu@gmail.com>
2020-07-01 13:31:31 -0400

    fix shcema.py class order issues

--- sibylapp/db/schema.py
+++ sibylapp/db/schema.py
@@ -15,6 +15,20 @@ from sibylapp.db.base import SibylAppDocument
 LOGGER = logging.getLogger(__name__)
 
 
+class Event(SibylAppDocument):
+    """Event object.
+
+    A **Event** represents ...
+    """
+    eid = fields.StringField()
+    datetime = fields.DateTimeField(required=True)
+    # TODO: choices from config
+    type = fields.StringField(required=True)
+    property = fields.DictField()  # {property:value}
+
+    # TODO: verify eid is in Entity collection
+
+
 class Entity(SibylAppDocument):
     """Entity object.
 
@@ -22,24 +36,12 @@ class Entity(SibylAppDocument):
     """
     eid = fields.StringField()
 
-    features = fields.ListField(fields.DictField()) # {feature:value}
-    property = fields.DictField() # {property:value}
+    features = fields.ListField(fields.DictField())  # {feature:value}
+    property = fields.DictField()  # {property:value}
 
     outcomes = fields.ListField(fields.ReferenceField(Event))  # contains Event objects
 
     unique_key_fields = ['eid']
-
-
-class Event(SibylAppDocument):
-    """Event object.
-
-    A **Event** represents ...
-    """
-    entity = fields.ReferenceField(Entity, required=True)
-    datetime = fields.DateTimeField(required=True)
-    # TODO: choices from config
-    type = fields.StringField(required=True)
-    property = fields.DictField() # {property:value}
 
 
 class Category(SibylAppDocument):
@@ -60,23 +62,6 @@ class Feature(SibylAppDocument):
     unique_key_fields = ['name']
 
 
-class Model(SibylAppDocument):
-    """Model object.
-
-    A **Model** represents ...
-    """
-    model = fields.BinaryField(required=True) # the model (must have model.predict())
-
-    name = fields.StringField()
-    description = fields.StringField()
-    performance = fields.StringField()
-    importances = fields.DictField()  # {feature_name:importance}
-    predictions = fields.DictField() # {entity_id:prediction}
-
-    explainer = fields.BinaryField() # trained contribution explainer
-    training_set = fields.ReferenceField(TrainingSet)
-
-
 class TrainingSet(SibylAppDocument):
     """Dataset object.
 
@@ -85,7 +70,21 @@ class TrainingSet(SibylAppDocument):
     # TODO: should we enforce 1-to-1 mappings between datasets and models for
     #  simplicity?
     entity_ids = fields.ListField(fields.ReferenceField(Entity))
-    neighbors = fields.BinaryField() # trained NN classifier
+    neighbors = fields.BinaryField()  # trained NN classifier
 
 
+class Model(SibylAppDocument):
+    """Model object.
 
+    A **Model** represents ...
+    """
+    model = fields.BinaryField(required=True)  # the model (must have model.predict())
+
+    name = fields.StringField()
+    description = fields.StringField()
+    performance = fields.StringField()
+    importances = fields.DictField()  # {feature_name:importance}
+    predictions = fields.DictField()  # {entity_id:prediction}
+
+    explainer = fields.BinaryField()  # trained contribution explainer
+    training_set = fields.ReferenceField(TrainingSet)
