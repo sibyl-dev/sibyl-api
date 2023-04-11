e7dbf70f22af6d524d753a0673efceeafe655611
zyteka <zyteka@mit.edu>
2023-04-11 15:02:47 -0400

    Bugfix

--- sibyl/sample_applications/prepare_housing_application.py
+++ sibyl/sample_applications/prepare_housing_application.py
@@ -3,6 +3,8 @@ from pyreal import RealApp
 import pickle
 
 x_orig, y_orig = ames_housing.load_data(include_targets=True)
+if "Id" in x_orig:  # TODO: remove once fixed in Pyreal's data
+    x_orig = x_orig.drop("Id", axis="columns")
 transformers = ames_housing.load_transformers()
 model = ames_housing.load_model()
 
