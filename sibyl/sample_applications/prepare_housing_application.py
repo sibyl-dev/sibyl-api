2929478632fdaf508bdc67fe2214d7dab491bbb7
zyteka <zyteka@mit.edu>
2023-04-24 14:38:49 -0400

    Updating model and explainer

--- sibyl/sample_applications/prepare_housing_application.py
+++ sibyl/sample_applications/prepare_housing_application.py
@@ -1,6 +1,10 @@
 from pyreal.sample_applications import ames_housing
 from pyreal import RealApp
 import pickle
+import os
+from sibyl.utils import get_project_root
+
+DIRECTORY = os.path.join(get_project_root(), "dbdata", "housing")
 
 x_orig, y_orig = ames_housing.load_data(include_targets=True)
 if "Id" in x_orig:  # TODO: remove once fixed in Pyreal's data
@@ -10,5 +14,5 @@ model = ames_housing.load_model()
 
 explainer = RealApp(model, X_train_orig=x_orig, y_orig=y_orig, transformers=transformers)
 
-pickle.dump(model, open("housing/model.pkl", "wb"))
-pickle.dump(explainer, open("housing/explainer.pkl", "wb"))
+pickle.dump(model, open(os.path.join(DIRECTORY, "model.pkl"), "wb"))
+pickle.dump(explainer, open(os.path.join(DIRECTORY, "explainer.pkl"), "wb"))
