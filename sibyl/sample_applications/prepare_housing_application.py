89a0b1536ca0b1a4da1b12161ddfce57a4c9c737
zyteka <zyteka@mit.edu>
2023-05-29 13:03:32 +0200

    Removing un-needed fit_transformer call

--- sibyl/sample_applications/prepare_housing_application.py
+++ sibyl/sample_applications/prepare_housing_application.py
@@ -4,7 +4,6 @@ import pandas as pd
 
 from pyreal import RealApp
 from pyreal.sample_applications import ames_housing
-from pyreal.transformers import fit_transformers
 
 from sibyl.utils import get_project_root
 
@@ -16,8 +15,6 @@ entities = pd.concat([x_orig, y_orig], axis=1)
 entities.to_csv(os.path.join(DIRECTORY, "entities.csv"), index=False)
 
 transformers = ames_housing.load_transformers()
-fit_transformers(transformers, x_orig)
-
 model = ames_housing.load_model()
 
 explainer = RealApp(model, transformers=transformers, id_column="eid")
