515b40313d2cd0de2eb76af50fa4feea4340f6bc
Ola Zytek <zyteka@mit.edu>
2023-11-27 16:19:16 -0500

    Small fix

--- sibyl/resources/computing.py
+++ sibyl/resources/computing.py
@@ -102,7 +102,7 @@ def get_entity_table(eid, row_id):
     if entity is None:
         LOGGER.exception("Error getting entity. Entity %s does not exist.", eid)
         return {"message": "Entity {} does not exist".format(eid)}, 400
-    return pd.DataFrame(get_features_for_row(entity.features, row_id), index=[0])
+    return pd.DataFrame(get_features_for_row(entity.features, row_id), index=[eid])
 
 
 def get_entities_table(eids, row_ids, all_rows=False):
